3fda6c4ed70e86341a608cf12cffe531ad09e327
AngelinaCholak <127952572+AngelinaCholak@users.noreply.github.com>
2023-11-04 13:54:29 +0400

    hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './styles.module.css';
 import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
@@ -17,42 +17,37 @@ export const App = () => {
   const [showModal, setShowModal] = useState(false);
   const [urlModal, setUrlModal] = useState(null);
 
-  const [prevPage, setPrevPage] = useState(1);
-  const [prevQuery, setPrevQuery] = useState('');
-
-  const fetchImages = useCallback(async () => {
-    try {
-      setSpinner(true);
-      const data = await fetchQuery(query, page);
-
-      if (data.hits.length === 0) {
-        toast.error('No images found for your query!');
-        setSpinner(false);
-        return;
-      }
-
-      const { hits: newImages, totalHits } = data;
-
-      if (page === 1) {
-        toast.info(`Found: ${totalHits} images for your request`);
-      }
-
-      setHits(prevHits => [...prevHits, ...newImages]);
-      setTotal(totalHits);
-      setSpinner(false);
-    } catch (error) {
-      toast.error('Error fetching data: ' + error);
-      setSpinner(false);
-    }
-  }, [query, page]);
-
   useEffect(() => {
-    if (page !== prevPage || query !== prevQuery) {
+    const fetchImages = async () => {
+      try {
+        setSpinner(true);
+        const data = await fetchQuery(query, page);
+
+        if (data.hits.length === 0) {
+          toast.error('No images found for your query!');
+          setSpinner(false);
+          return;
+        }
+
+        const { hits: newImages, totalHits } = data;
+
+        if (page === 1) {
+          toast.info(`Found: ${totalHits} images for your request`);
+        }
+
+        setHits(prevHits => [...prevHits, ...newImages]);
+        setTotal(totalHits);
+        setSpinner(false);
+      } catch (error) {
+        toast.error('Error fetching data: ' + error);
+        setSpinner(false);
+      }
+    };
+
+    if (page > 1 || query) {
       fetchImages();
-      setPrevPage(page);
-      setPrevQuery(query);
     }
-  }, [page, query, prevPage, prevQuery, fetchImages]);
+  }, [page, query]);
 
   const handleFormSubmit = newQuery => {
     if (query === newQuery) {
@@ -67,8 +62,8 @@ export const App = () => {
     setPage(prevPage => prevPage + 1);
   };
 
-  const openModal = newUrlModal => {
-    setUrlModal(newUrlModal);
+  const openModal = imgUrl => {
+    setUrlModal(imgUrl);
     setShowModal(true);
   };
 
